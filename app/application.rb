b56225b4d683915b7847cbe4d38f0d4c8ba753a9
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-16 02:45:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -25,7 +25,7 @@ class Application
       if @@items.include?(item_to_add)
         @@cart << item_to_add
         resp.write "added #{item_to_add}"
-      else 
+      else
         resp.write "We don't have that item"
       end
     else
